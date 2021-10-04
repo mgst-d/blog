2cac258ab0f12cc33585b90c724aeb523560e6ab
mgst-d <and8511@ya.ru>
2021-10-04 20:51:54 +0300

    Correct contact's form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   resources :articles
-  get 'contacts/new'
-  post 'contacts/create'
+  #get 'contacts/new'
+  #post 'contacts/create'
+  resource :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
