bdd003e8442eaeb072c242e1bed678e13d911e86
mgst-d <and8511@ya.ru>
2021-10-04 21:18:40 +0300

    Save contact to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,14 @@ class ContactsController < ApplicationController
   def new
   end
   def create
+    @c = Contact.new contact_params
+    @c.save
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
