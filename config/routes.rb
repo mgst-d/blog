a61bc1af9944b829b116a6f948d2fdb2a61bcda7
mgst-d <and8511@ya.ru>
2021-10-06 20:29:16 +0300

    Add contacts errors_messages, about, and update tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'about/index'
   get 'home/index'
   resources :articles
   #get 'contacts/new'
--- task.rb
+++ task.rb
@@ -2,17 +2,25 @@ f = File.open "text.txt", "r"
 h = {}
 
 f.each_line do |s|
-  a = s.split(/\s|\n|\.|,|\-/)
+  a = s.split(/\s|\n|\.|,|\-| /)
   a.each do |a1|
     #if h.keys.include? a1
-      h[a1] = h[a1].to_i + 1
+      h[a1] = h[a1].to_i + 1 if a1 != ''
     #else
     #  h[a1] = 1
     #end
   end
 end
-h.each do |k, v|
-  print "#{v} #{k}-->"
-end
 
+i = 0
+h.each do |k, v|
+   i = v if v > i
+end
+while i > 0 do
+  h.each do |k, v|
+    puts "#{v} #{k}-->" if v == i
+  end
+  i = i - 1
+end
+#puts i
 f.close
