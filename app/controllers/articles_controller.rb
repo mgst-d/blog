d955566120bef49b09e09fe6ba8574e5b55b0957
mgst-d <and8511@ya.ru>
2021-10-06 23:48:29 +0300

    Save article to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,20 @@
 class ArticlesController < ApplicationController
   def new
+
   end
   def create
-    render plain: params[:article].inspect
+    @c = Article.new article_params
+    if @c.valid?
+      @c.save
+    else
+      render action: 'new'
+    end
+    #render plain: params[:article].inspect
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
