dea3e18243d1df8870dd14e80f1aa4b0dd3a64b4
mgst-d <and8511@ya.ru>
2021-10-09 20:56:26 +0300

    Add show and show all methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
   def new
 
   end
@@ -6,10 +11,15 @@ class ArticlesController < ApplicationController
     @c = Article.new article_params
     if @c.valid?
       @c.save
+      redirect_to @c
     else
       render action: 'new'
     end
     #render plain: params[:article].inspect
+  end
+
+  def show
+    @article = Article.find params[:id]
   end
 
   private
@@ -17,4 +27,5 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :text)
   end
+  
 end
