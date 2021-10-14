4cc304bdaa9bf1989c8271ef0e199ce9d00bc95d
mgst-d <and8511@ya.ru>
2021-10-14 20:02:36 +0300

    Add edit method and pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   #get 'contacts/new'
   #post 'contacts/create'
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
+
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
