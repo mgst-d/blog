9bb27522c5523a6dec4623ec88e01e2c2437587f
mgst-d <and8511@ya.ru>
2021-10-25 23:20:52 +0300

    Add comment's form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'about/index'
   get 'home/index'
   resources :articles do
-    resources :comments
+    resources :comments, only: [:create]
   end
 
   #get 'contacts/new'
