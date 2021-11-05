6199474c5010670e7baf6e561b7d63318e1ed982
mgst-d <and8511@ya.ru>
2021-11-05 16:08:14 +0300

    Correct form devise:views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
   def create
     @article = Article.find params[:article_id]
     @article.comments.create(comment_params)
+
+    # comment = @article.comments.new author: current_user.username, :body
+    # comment.save(comment_params)
 
     redirect_to article_path(@article)
 
